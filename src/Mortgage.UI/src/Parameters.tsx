6f9ef373415e14ae074facd69251f9a5d5e2f705
Simon Thomas <simon.thomas1978@gmail.com>
2020-12-22 13:47:38 +0000

    switched to using Formik framework

--- src/Mortgage.UI/src/Parameters.tsx
+++ src/Mortgage.UI/src/Parameters.tsx
@@ -1,99 +1,100 @@
-import React, { ChangeEvent, FormEvent, useContext } from 'react'
+import React, {useContext} from "react"
+import { Formik, useField } from "formik"
 import {Context} from './Provider'
 import { Form, Button, Row, Col } from 'react-bootstrap'
-import { ActionType } from './AmortizationReducer'
+import * as Yup from 'yup'
 
+interface Props{
+    label:string;
+    name:string;
+    type:string;
+    placeholder:string
+}
+
+const InputText = (props:Props) => {
+    const [field, meta] = useField(props)
+
+    return (
+        <Form.Group as={Row} controlId={props.name}>
+            <Form.Label column sm={1}>{props.label}</Form.Label>
+            <Col sm={2}>
+                <Form.Control {...field} {...props}
+                    isValid={meta.touched && !meta.error} isInvalid={meta.touched && (meta.error != null)} />
+            </Col>
+        </Form.Group>
+    )
+}
 
 const Parameters = () => {
-    const {state, dispatch, calculateAmortization} = useContext(Context)
-
-    const handleLoanChange = (evt: ChangeEvent<HTMLInputElement>) => {
-        dispatch({
-            type: ActionType.LoanChanged,
-            payload: evt.target.value
-        });
-    }
-
-    const handlePeriodChange = (evt: ChangeEvent<HTMLInputElement>) => {
-        dispatch({
-            type: ActionType.PeriodChanged,
-            payload: evt.target.value
-        });
-    }
-
-    const handleRateChange = (evt: ChangeEvent<HTMLInputElement>) => {
-        dispatch({
-            type: ActionType.RateChanged,
-            payload: evt.target.value
-        });
-    }
-
-    const handleLoanTouched = (evt: ChangeEvent<HTMLInputElement>) => {
-        dispatch({
-            type: ActionType.LoanTouched,
-            payload: ''
-        });
-    }
-
-    const handlePeriodTouched = (evt: ChangeEvent<HTMLInputElement>) => {
-        dispatch({
-            type: ActionType.PeriodTouched,
-            payload: ''
-        });
-    }
-
-    const handleRateTouched = (evt: ChangeEvent<HTMLInputElement>) => {
-        dispatch({
-            type: ActionType.RateTouched,
-            payload: ''
-        });
-    }
-
-    const canBeSubmitted = () => !state.errors.loan && !state.errors.period && !state.errors.rate
-
-    const onSubmit = (evt: FormEvent) => {
-        evt.preventDefault()
-
-        if (!canBeSubmitted()) {
-            return
-        }
-
-        calculateAmortization({
-            loan: Number(state.loan),
-            period: Number(state.period),
-            rate: Number(state.rate)});
-    }
+    const {calculateAmortization} = useContext(Context)
 
     return (
         <div>
-            <Form onSubmit={onSubmit}>
-                <Form.Group as={Row} controlId="loan">
-                    <Form.Label column sm={1}>Loan</Form.Label>
-                    <Col sm={2}>
-                        <Form.Control placeholder="Loan" value={state.loan} onChange={handleLoanChange} onBlur={handleLoanTouched}
-                            isValid={state.touched.loan && !state.errors.loan} isInvalid={state.touched.loan && state.errors.loan} />
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="period">
-                    <Form.Label column sm={1}>Period</Form.Label>
-                    <Col sm={2}>
-                        <Form.Control placeholder="Period" value={state.period} onChange={handlePeriodChange} onBlur={handlePeriodTouched}
-                            isValid={state.touched.period && !state.errors.period} isInvalid={state.touched.period && state.errors.period} />
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} controlId="rate">
-                    <Form.Label column sm={1}>Rate</Form.Label>
-                    <Col sm={2}>
-                        <Form.Control placeholder="Rate" value={state.rate} onChange={handleRateChange} onBlur={handleRateTouched}
-                           isValid={state.touched.rate && !state.errors.rate} isInvalid={state.touched.rate && state.errors.rate} />
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row}>
-                    <Col sm={{ span: 3, offset: 1 }}>
-                        <Button type="submit" variant={!canBeSubmitted() ? 'warning' : 'success'} disabled={!canBeSubmitted()}>Calculate</Button>
-                    </Col>
-                </Form.Group>
-            </Form>
+            <Formik
+                initialValues={{
+                    loan: '',
+                    period: '',
+                    rate: ''
+                }}
+                validationSchema={Yup.object({
+                    loan: Yup.number()
+                        .required('Required'),
+                    period: Yup.number()
+                        .required('Required'),
+                    rate: Yup.number()
+                        .required('Required')
+                })}
+                onSubmit={(values, {setSubmitting}) => {
+                    setSubmitting(true);
+
+                    calculateAmortization({
+                        loan: Number(values.loan),
+                        period: Number(values.period),
+                        rate: Number(values.rate)});
+
+                    setSubmitting(false);
+                }}
+            >
+                {({
+                    handleSubmit,
+                    isValid,
+                    dirty
+                }) => (
+                    <Form onSubmit={(e) => {
+                            e.preventDefault();
+                            handleSubmit()
+                    }}>
+                        <InputText
+                            label="Loan"
+                            name="loan"
+                            type="text"
+                            placeholder="Enter loan amount"
+                        />
+
+                        <InputText
+                            label="Period"
+                            name="period"
+                            type="text"
+                            placeholder="Enter period"
+                        />
+
+                        <InputText
+                            label="Rate"
+                            name="rate"
+                            type="text"
+                            placeholder="Enter rate"
+                        />
+
+                        <Form.Group as={Row}>
+                            <Col sm={{ span: 3, offset: 1 }}>
+                                <Button  type="submit" 
+                                    variant={!(dirty && isValid) ? 'warning' : 'success'} 
+                                    disabled={!(dirty && isValid)}>Calculate</Button>
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
